5342276f8cd054fd91d97df5982349485b42df68
Thomas Erickson <thomas@mixbook.com>
2022-01-24 16:50:57 -0700

    Progress

--- ext/rb_ckmeans/extconf.rb
+++ ext/rb_ckmeans/extconf.rb
@@ -1,3 +1,10 @@
 require "mkmf"
 
+dir_config "rb_ckmeans/rb_ckmeans"
+
+$srcs = Dir.glob("#{$srcdir}/**/*.cpp").map { |path| File.basename(path) }
+$VPATH << "$(srcdir)/src"
+$INCFLAGS << " -I$(srcdir)/src"
+$CPPFLAGS << " -std=c++11"
+
 create_makefile "rb_ckmeans/rb_ckmeans"
--- ext/rb_ckmeans/rb_ckmeans.cpp
+++ ext/rb_ckmeans/rb_ckmeans.cpp
@@ -6,9 +6,38 @@
 VALUE rb_ckmeans_od_dp(VALUE self, VALUE in_array)
 {
   long len = rb_array_len(in_array);
-  VALUE entry = rb_ary_entry(in_array, 0);
 
-  printf("od_dp %ld %s\n", len, StringValuePtr(entry));
+  double* values = new double(len);
+  for (int i = 0; i < len; i++) {
+    VALUE entry = rb_ary_entry(in_array, i);
+    
+    values[i] = rb_num2dbl(entry);
+    printf("rb_ckmeans_od_dp input %i %lf\n", i, values[i]);
+  }
+
+  int* cluster = new int(len);
+  double* centers = new double(len);
+  double* withinss = new double(len);
+  double* size = new double(len);
+  double* BICs = new double(len);
+  printf("%p %p %p %p %p %p\n", cluster, centers, withinss, size, BICs);
+
+  kmeans_1d_dp(values, len, NULL, 1, len, cluster, centers, withinss, size, BICs, "BIC 3.4.12", "linear", L1);
+
+  printf("%p %p %p %p %p %p\n", cluster, centers, withinss, size, BICs);
+
+  for (int i = 0; i < len; i++) {
+    printf("rb_ckmeans_od_dp output %i %d\n", i, cluster[i]);
+  }
+
+ /*
+  delete values;
+  delete cluster;
+  delete centers;
+  delete withinss;
+  delete size;
+  delete BICs;
+  */
 
   return Qnil;
 }
